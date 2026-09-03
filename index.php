8ff3dd1de36f0ca35eac8e4b81b54cab94f2fa73
Jake <167881717+Jeii22@users.noreply.github.com>
2026-09-03 15:37:16 +0800

    test font

--- index.php
+++ index.php
@@ -101,7 +101,7 @@
     }
 
     .hero-title {
-      font-family: 'Playfair Display', serif;
+      font-family: 'Rustic Roadway', serif;
       font-size: clamp(2.8rem, 8vw, 5.5rem);
       font-weight: 700;
       font-style: italic;
